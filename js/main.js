4bcc063e93f5fbe17e05e3046b0553c11e6cade7
dustyxcat <smr623@naver.com>
2025-10-28 10:59:36 +0900

    sec_1 end
    + header padding

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
 
+    var swiper = new Swiper(".sec_1_swiper", {
+        loop: true,
+        autoplay: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
 }); //end
 
