5a23d811d0bf5955b30e6f8b2427d775324d4890
dustyxcat <smr623@naver.com>
2025-10-29 09:47:57 +0900

    sec_5 end sec_6 video

--- js/main.js
+++ js/main.js
@@ -10,15 +10,43 @@ $(document).ready(function(){
     });
 
     var swiper = new Swiper(".news_wrap", {
-      slidesPerView: 1.2,
+      slidesPerView: 1.5,
       spaceBetween: 15,
       freeMode: true,
     });
 
     var swiper = new Swiper(".story_wrap", {
-      slidesPerView: 1.2,
+      slidesPerView: 1.5,
       spaceBetween: 15,
       freeMode: true,
+    });
+
+    $('.title_news').click(function(){
+
+      $('.title_5 h6').addClass('active_color')
+      $('.under_bar').addClass('active_bar')
+
+      $('.story_wrap').addClass('active_none')
+
+      $('.title_story h6').removeClass('active_color')
+      $('.title_story .under_bar').removeClass('active_bar')
+
+      $('.news_wrap').removeClass('active_none')
+
+    });
+
+    $('.title_story').click(function(){
+
+      $('.title_5 h6').addClass('active_color')
+      $('.under_bar').addClass('active_bar')
+
+      $('.news_wrap').addClass('active_none')
+
+      $('.title_news h6').removeClass('active_color')
+      $('.title_news .under_bar').removeClass('active_bar')
+
+      $('.story_wrap').removeClass('active_none')
+
     });
 
 
